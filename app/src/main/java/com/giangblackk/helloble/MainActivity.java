95af6618a30ec85984b161a9116a276a814c6069
giangblackk <giangblackk@gmail.com>
2021-05-28 15:20:54 +0700

    simple button and text

--- app/src/main/java/com/giangblackk/helloble/MainActivity.java
+++ app/src/main/java/com/giangblackk/helloble/MainActivity.java
@@ -1,14 +1,26 @@
 package com.giangblackk.helloble;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements LifecycleOwner {
+    int number = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        TextView textview = findViewById(R.id.textView);
+        textview.setText(String.valueOf(number));
+        button.setOnClickListener(v -> {
+            number = number + 1;
+            textview.setText(String.valueOf(number));
+        });
     }
 }
