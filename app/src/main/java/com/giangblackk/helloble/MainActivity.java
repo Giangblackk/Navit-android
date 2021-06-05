eb181dc97e9c7b8f25a1cdce42c56c0a615aae27
giangblackk <giangblackk@gmail.com>
2021-06-05 13:23:46 +0700

    add MapView to Activity

--- app/src/main/java/com/giangblackk/helloble/MainActivity.java
+++ app/src/main/java/com/giangblackk/helloble/MainActivity.java
@@ -1,5 +1,6 @@
 package com.giangblackk.helloble;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
 
@@ -8,12 +9,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+
 public class MainActivity extends AppCompatActivity implements LifecycleOwner {
     int number = 0;
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        // something to do here
+                    }
+                });
+            }
+        });
 
         Button button = findViewById(R.id.button);
         TextView textview = findViewById(R.id.textView);
@@ -22,5 +51,47 @@ public class MainActivity extends AppCompatActivity implements LifecycleOwner {
             number = number + 1;
             textview.setText(String.valueOf(number));
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
